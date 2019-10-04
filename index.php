06e40a8029dfba8c874f77e215dc8b7312410fca
uguroktay <56156686+uguroktay@users.noreply.github.com>
2019-10-04 14:18:13 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+
+<html>
+
+<head>
+
+<title>Ansible Essentials</title>
+
+</head>
+
+<body>
+
+<h1>php from another site !</h1>
+
+<p> App backend <?=gethostname()?> </p>
+
+<img src="http://fillmurray.com/408/287" />
+
+</body>
+
+</html>
