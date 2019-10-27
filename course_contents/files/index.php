cfe0f493c6c7804139d49ab22b80016451dce2fb
uguroktay <56156686+uguroktay@users.noreply.github.com>
2019-10-27 18:30:35 +0300

    Add files via upload

---
+++ course_contents/files/index.php
@@ -0,0 +1,20 @@
+
+
+<html>
+
+<head>
+
+<title>Ansible Essentials</title>
+
+</head>
+
+<body>
+
+<h1>Welcome to Ansible Essentials Course !</h1>
+<p> App backend <?=gethostname()?> </p>
+
+<img src="https://loremflickr.com/320/240" />
+
+</body>
+
+</html>
